d4f3d03b6d8274d80a24a13fd15619eb264fa9d2
Alexis <alexis.escudero195@gmail.com>
2022-01-17 22:00:37 +0100

    l

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@/': new URL('./src/', import.meta.url).pathname
+      '@/dist/': new URL('./src/', import.meta.url).pathname
     }
   }
 })
